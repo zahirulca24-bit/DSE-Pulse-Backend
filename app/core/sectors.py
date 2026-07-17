a43cc7966f5e3f4ecb576151b326a2cc80f907dd
Md. Zahirul Islam <zahirul.ca24@gmail.com>
2026-07-17 17:40:57 +0600

    feat: add approved DSE sectors

---
+++ app/core/sectors.py
@@ -0,0 +1,45 @@
+"""Approved DSE sector names for the current application phase."""
+
+from typing import Literal
+
+SectorName = Literal[
+    "Bank",
+    "Cement",
+    "Ceramics",
+    "Engineering",
+    "Financial Institutions",
+    "Food & Allied",
+    "Fuel & Power",
+    "Insurance",
+    "IT Sector",
+    "Jute",
+    "Miscellaneous",
+    "Paper & Printing",
+    "Pharmaceuticals & Chemicals",
+    "Services & Real Estate",
+    "Tannery Industries",
+    "Telecommunication",
+    "Textile",
+    "Travel & Leisure",
+]
+
+APPROVED_DSE_SECTORS: tuple[SectorName, ...] = (
+    "Bank",
+    "Cement",
+    "Ceramics",
+    "Engineering",
+    "Financial Institutions",
+    "Food & Allied",
+    "Fuel & Power",
+    "Insurance",
+    "IT Sector",
+    "Jute",
+    "Miscellaneous",
+    "Paper & Printing",
+    "Pharmaceuticals & Chemicals",
+    "Services & Real Estate",
+    "Tannery Industries",
+    "Telecommunication",
+    "Textile",
+    "Travel & Leisure",
+)
